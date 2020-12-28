805d99f1ff375e25ff8935a1f21166cea76a8af8
unknown <thom.florian.1997@googlemail.com>
2020-12-28 17:36:18 +0100

    Continued to integrate typescript

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,8 +2,12 @@ import "../styles/globals.css";
 import type { AppProps /*, AppContext */ } from "next/app";
 import { GetServerSideProps, NextPage } from "next";
 
-function MyApp({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />;
-}
+// function MyApp({ Component, pageProps }: AppProps) {
+//     return <Component {...pageProps} />;
+// }
+
+const MyApp: NextPage<AppProps> = (props: AppProps) => {
+    return <props.Component {...props.pageProps} />;
+};
 
 export default MyApp;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,16 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import { GetServerSideProps, NextPage } from "next";
+import React from "react";
+import type { AppProps } from "next/app";
 
 interface Props {
-    launch?: {
-        mission: string;
+    test?: {
+        test: string;
     };
 }
 
-const IndexPage: NextPage<Props> = (props) => {
+const IndexPage: NextPage<Props> = (props: Props) => {
     return (
         <div className={styles.container}>
             <Head>
