b1bd8c30d6f89c2b851439ac6ad80a0fc2855c91
unknown <thom.florian.1997@googlemail.com>
2020-12-28 14:31:44 +0100

    Continued eslint+prettier integration

--- README.md
+++ README.md
@@ -4,21 +4,19 @@
 
 Frontend-Project to create my personal website. The backend-code can be found [here].
 
-
-
 ## Prerequisites
 
-- yarn v1.22+
+-   yarn v1.22+
 
 ## Getting Started
 
-```$ git clone git@github.com:FlorianTh2/Personal-Website.git```
+`$ git clone git@github.com:FlorianTh2/Personal-Website.git`
 
-```$ cd ./Personal-Website```
+`$ cd ./Personal-Website`
 
-```$ yarn install```
+`$ yarn install`
 
-```$ yarn dev```
+`$ yarn dev`
 
 ## Important commands
 
@@ -30,16 +28,16 @@ Frontend-Project to create my personal website. The backend-code can be found [h
 
 ## Build with
 
-- React v17
-- Next.js
-- TypeScript
-- Redux
-- Redux-Thunk
-- ESLint / Prettier
-- Tailwindcss v2
+-   React v17
+-   Next.js
+-   TypeScript
+-   Redux
+-   Redux-Thunk
+-   ESLint / Prettier
+-   Tailwindcss v2
 
 ## Acknowledgements
+
 --
 
-
-[here]: <https://github.com/FlorianTh2/homepageBackend>
+[here]: https://github.com/FlorianTh2/homepageBackend
--- pages/index.js
+++ pages/index.js
@@ -29,16 +29,15 @@ export default function Home() {
                         <p>Learn about Next.js in an interactive course with quizzes!</p>
                     </a>
 
-                    <a
-                        href="https://github.com/vercel/next.js/tree/master/examples"
-                        className={styles.card}>
+                    <a href="https://github.com/vercel/next.js/tree/master/examples" className={styles.card}>
                         <h3>Examples &rarr;</h3>
                         <p>Discover and deploy boilerplate example Next.js projects.</p>
                     </a>
 
                     <a
                         href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-                        className={styles.card}>
+                        className={styles.card}
+                    >
                         <h3>Deploy &rarr;</h3>
                         <p>Instantly deploy your Next.js site to a public URL with Vercel.</p>
                     </a>
@@ -49,7 +48,8 @@ export default function Home() {
                 <a
                     href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
                     target="_blank"
-                    rel="noopener noreferrer">
+                    rel="noopener noreferrer"
+                >
                     Powered by <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
                 </a>
             </footer>
