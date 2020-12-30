af638c73438fb899c9a7cec785da255738263c38
unknown <thom.florian.1997@googlemail.com>
2020-12-30 23:21:10 +0100

    Fixed tailwind postcss purge

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
     purge: {
-        enabled: true,
+        enabled: process.env.ENVIRONMENT === "production",
         content: ["./src/**/*.tsx"],
     },
     darkMode: false, // or 'media' or 'class'
