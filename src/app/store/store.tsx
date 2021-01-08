cc8a4d730287e8eeb1a2cd9605fe343186511513
unknown <thom.florian.1997@googlemail.com>
2021-01-08 04:39:03 +0100

    Turned of redux-toolkit devtools for production

--- src/app/store/store.tsx
+++ src/app/store/store.tsx
@@ -6,7 +6,15 @@ const middleware = [...getDefaultMiddleware()];
 export const store = configureStore({
     reducer: rootReducer,
     middleware: middleware,
+    devTools: process.env.NODE_ENV != "production",
 });
+
+if (process.env.NODE_ENV === "development" && module.hot) {
+    module.hot.accept("@/src/app/store/reducers/rootReducer", () => {
+        const newRootReducer = require("@/src/app/store/reducers/rootReducer").default;
+        store.replaceReducer(newRootReducer);
+    });
+}
 
 export type RootState = ReturnType<typeof store.getState>;
 
