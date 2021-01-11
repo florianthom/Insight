55119cd1f91d4de7778e540136424dc7a4d64e57
unknown <thom.florian.1997@googlemail.com>
2021-01-11 15:56:38 +0100

    Changed name of test tsconfig

--- jest.config.js
+++ jest.config.js
@@ -20,9 +20,9 @@ module.exports = {
     globals: {
         // we must specify a custom tsconfig for tests because we need the typescript transform
         // to transform jsx into js rather than leaving it jsx such as the next build requires.  you
-        // can see this setting in tsconfig.jest.json -> "jsx": "react"
+        // can see this setting in tsconfig.spec.json -> "jsx": "react"
         "ts-jest": {
-            tsConfig: "tsconfig.jest.json",
+            tsConfig: "tsconfig.spec.json",
         },
     },
 };
