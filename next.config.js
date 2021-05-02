67208cfe60a9459bac2e76c023773a2016a776de
unknown <thom.florian.1997@googlemail.com>
2021-05-02 23:34:54 +0200

    Extended secret management, for kubernetes also

--- next.config.js
+++ next.config.js
@@ -1,8 +1,1 @@
-console.log(process.env.ENVIRONMENT);
-
-module.exports = {
-    publicRuntimeConfig: {
-        NODE_ENV: process.env.ENVIRONMENT,
-        PORT: process.env.PORT,
-    },
-};
+module.exports = {};
--- tailwind.config.js
+++ tailwind.config.js
@@ -14,7 +14,6 @@ const custom_colors = {
 
 module.exports = {
     purge: {
-        enabled: process.env.ENVIRONMENT === "production",
         content: ["./src/**/*.{tsx,js}"],
     },
     darkMode: false, // or 'media' or 'class'
