17cb8c58cc058c7a474b40f89bec9c9ca97bfc3d
unknown <thom.florian.1997@googlemail.com>
2021-05-02 23:44:15 +0200

    Added environment variables to constants module to get them easier

--- src/app/shared/constants/constants.ts
+++ src/app/shared/constants/constants.ts
@@ -1,3 +1,7 @@
+export const __prod__ = process.env.NODE_ENV === "production";
+export const baseUrl = process.env.Base_URL;
+export const apiBaseUrl = process.env.API_BASE_URL;
+
 export const email_string = "mail@florianthom.io";
 export const github_link = "https://github.com/FlorianTh2";
 export const discord_link = "https://discord.gg/UYq6fz8TVw";
