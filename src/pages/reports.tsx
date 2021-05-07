101d67c93d4018d25be175dae4427299f72ef773
unknown <thom.florian.1997@googlemail.com>
2021-05-07 11:46:42 +0200

    Integrated server-side-rendering

--- src/pages/reports.tsx
+++ src/pages/reports.tsx
@@ -3,8 +3,21 @@ import React from "react";
 import Head from "next/head";
 import { Work } from "@/src/app/work/work";
 import { Reports } from "@/src/app/reports/reports";
+import { defaultQueryClient } from "@/src/app/shared/queryClient/defaultQueryClient";
+import { firstPage, pageSize } from "@/src/app/shared/constants/constants";
+import { dehydrate } from "react-query/hydration";
 
 interface Props {}
+
+export async function getStaticProps(): Promise<any> {
+    const queryClient = defaultQueryClient;
+    await queryClient.prefetchQuery("/documents");
+    return {
+        props: {
+            dehydratedState: dehydrate(queryClient),
+        },
+    };
+}
 
 const ReportsPage: NextPage<Props> = (props: Props) => {
     return (
