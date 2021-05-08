66d52582b665f7ed8dcccb4f90d17bb22119f154
unknown <thom.florian.1997@googlemail.com>
2021-05-08 19:13:33 +0200

    Integrated max pages number in projects table

--- src/app/projects/repositoryTable/repositoryTable.tsx
+++ src/app/projects/repositoryTable/repositoryTable.tsx
@@ -34,7 +34,7 @@ export const RepositoryTable: React.FC<Props> = (props: Props) => {
                 </thead>
                 <tfoot>
                     <tr className="bottom-box-shadow">
-                        <td colSpan={4}>
+                        <td colSpan={6}>
                             <div className="flex justify-between items-center">
                                 <button
                                     onClick={() => {
@@ -43,12 +43,18 @@ export const RepositoryTable: React.FC<Props> = (props: Props) => {
                                         }
                                     }}
                                     disabled={projectsData === undefined || !projectsData.previousPage}
-                                    className="bg-gray-100 hover:bg-gray-200 border py-4 m-1 w-3/12 disabled:opacity-50 disabled:cursor-default"
+                                    className="bg-gray-100 hover:bg-gray-200 border py-4 m-1 w-4/12 disabled:opacity-50 disabled:cursor-default"
                                 >
                                     Previous
                                 </button>
                                 <div>
-                                    <span>Page {page}</span>
+                                    <span>
+                                        Page{" "}
+                                        <span className="bg-white rounded border border-gray-300 px-4 py-2">
+                                            {page}
+                                        </span>{" "}
+                                        of {projectsData.pagesTotal}
+                                    </span>
                                 </div>
                                 <button
                                     onClick={() => {
@@ -57,7 +63,7 @@ export const RepositoryTable: React.FC<Props> = (props: Props) => {
                                         }
                                     }}
                                     disabled={projectsData === undefined || !projectsData.nextPage}
-                                    className="bg-gray-100 hover:bg-gray-200 border py-4 m-1 w-3/12 disabled:opacity-50 disabled:cursor-default"
+                                    className="bg-gray-100 hover:bg-gray-200 border py-4 m-1 w-4/12 disabled:opacity-50 disabled:cursor-default"
                                 >
                                     Next
                                 </button>
