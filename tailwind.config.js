3285bc31c9771934ab617af591a7b7ce94c92622
unknown <thom.florian.1997@googlemail.com>
2022-04-10 19:38:29 +0200

    Upgraded tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,39 +1,23 @@
-const defaultTheme = require("tailwindcss/defaultTheme");
-
-const custom_colors = {
-    ...defaultTheme.colors,
-    ...{
-        "main-color": {
-            DEFAULT: "#1B262C",
-        },
-        "main-color-light": {
-            DEFAULT: "#313b41",
-        },
-    },
-};
-
-const customTracking = {
-    ...defaultTheme.letterSpacing,
-    ...{
-        headline: "0.5em",
-        "headline-mobile": "0.35em",
-    },
-};
-
 module.exports = {
-    purge: {
-        content: ["./src/**/*.{tsx,js}"],
-    },
-    darkMode: false, // or 'media' or 'class'
+    content: [
+        // Example content paths...
+        "./public/**/*.html",
+        "./src/**/*.{js,jsx,ts,tsx,vue}",
+    ],
     theme: {
-        colors: custom_colors,
-        letterSpacing: customTracking,
-    },
-    variants: {
-        backgroundColor: ["responsive", "hover", "focus", "active"],
         extend: {
-            opacity: ["disabled"],
-            cursor: ["disabled"],
+            colors: {
+                "main-color": {
+                    DEFAULT: "#1B262C",
+                },
+                "main-color-light": {
+                    DEFAULT: "#313b41",
+                },
+            },
+            letterSpacing: {
+                headline: "0.5em",
+                "headline-mobile": "0.35em",
+            },
         },
     },
     plugins: [],
