a9edbeda87513dbbd1dfe8f8348bba4829d7c3ca
unknown <thom.florian.1997@googlemail.com>
2022-05-18 22:39:51 +0200

    Several changes

--- src/app/projects/repositoryTable/repositoryTable.tsx
+++ src/app/projects/repositoryTable/repositoryTable.tsx
@@ -1,18 +1,24 @@
 import React, { useState } from "react";
 import { useQuery } from "react-query";
-import { PagedResponseProject } from "@/src/openapi_models/models/PagedResponseProject";
 import { Project } from "@/src/app/shared/models/Project";
-import { firstPage, pageSize } from "@/src/app/shared/constants/constants";
+import * as constants from "@/src/app/shared/constants/constants";
 import { BasicSpinner } from "@/src/app/shared/basicSpinner/basicSpinner";
+import {
+    getNumberPages,
+    nextPageAvailable,
+    paginate,
+    previousPageAvailable
+} from "@/src/app/shared/utils/paginationUtils";
+import {Document} from "@/src/app/shared/models/Document";
+import data from "@/public/static/data/Projects.json";
 
 interface Props {}
 
 export const RepositoryTable: React.FC<Props> = (props: Props) => {
-    const [page, setPage] = useState(firstPage);
-    const { isLoading, error, data: projectsData } = useQuery<PagedResponseProject, Error>(
-        "/projects?pagesize=" + pageSize + "&pagenumber=" + page,
-        { keepPreviousData: true },
-    );
+    const projectsData: Project[] = data;
+    const [page, setPage] = useState(constants.firstPage);
+    const pagedData = paginate(projectsData, constants.pageSize, page)
+
 
     return (
         <div className="overflow-hidden">
@@ -35,48 +41,40 @@ export const RepositoryTable: React.FC<Props> = (props: Props) => {
                         </tr>
                     </thead>
                     <tbody className="divide-y divide-gray-200">
-                        {isLoading ? (
-                            <tr>
-                                <td colSpan={4}>
-                                    <div className="flex justify-center px-4 py-4">
-                                        <BasicSpinner></BasicSpinner>
-                                    </div>
-                                </td>
-                            </tr>
-                        ) : (
-                            projectsData?.data
+                        {
+                            pagedData
                                 ?.sort((a, b) =>
                                     new Date(a.projectCreatedOn) >= new Date(b.projectCreatedOn) ? -1 : 1,
                                 )
                                 .map((a: Project) => (
-                                    <tr key={a.id} className="divide-x divide-gray-100">
+                                    <tr key={a.Id} className="divide-x divide-gray-100">
                                         <td className="px-2 pt-4 pb-2 whitespace-nowrap overflow-hidden overflow-ellipsis">
                                             <a
-                                                href={a.htmLUrl}
+                                                href={a.HtmLUrl}
                                                 target="_blank"
                                                 rel="noopener noreferrer"
                                                 className="transition cursor-pointer hover:underline"
                                             >
-                                                {a.name}
+                                                {a.Name}
                                             </a>
                                         </td>
                                         <td className="px-2 pt-4 pb-2 whitespace-nowrap overflow-hidden overflow-ellipsis">
-                                            {a.description}
+                                            {a.Description}
                                         </td>
                                         <td className="px-2 pt-4 pb-2 whitespace-nowrap overflow-hidden overflow-ellipsis">
-                                            {a.language}
+                                            {a.Language}
                                         </td>
                                         <td className="px-2 pt-4 pb-2 whitespace-nowrap overflow-hidden overflow-ellipsis">
-                                            {a.visibility}
+                                            {a.Visibility}
                                         </td>
                                         <td className="px-2 pt-4 pb-2 whitespace-nowrap overflow-hidden overflow-ellipsis">
-                                            {a.size}
+                                            {a.Size}
                                         </td>
                                         <td className="px-2 pt-4 pb-2 whitespace-nowrap overflow-hidden overflow-ellipsis">
-                                            {new Date(a.projectCreatedOn).toLocaleDateString("en-US")}
+                                            {new Date(a.ProjectCreatedOn).toLocaleDateString("en-US")}
                                         </td>
                                     </tr>
-                                ))
+                                )
                         )}
                     </tbody>
                 </table>
@@ -84,11 +82,11 @@ export const RepositoryTable: React.FC<Props> = (props: Props) => {
             <div className="flex justify-between items-center border-l border-r border-b border-gray-300 shadow">
                 <button
                     onClick={() => {
-                        if (projectsData.previousPage) {
+                        if (previousPageAvailable(page)) {
                             setPage((a) => a - 1);
                         }
                     }}
-                    disabled={projectsData === undefined || !projectsData.previousPage}
+                    disabled={!previousPageAvailable(page)}
                     className="bg-gray-100 hover:bg-gray-200 border py-4 m-1 w-3/12 disabled:opacity-50 disabled:cursor-default"
                 >
                     Previous
@@ -96,16 +94,16 @@ export const RepositoryTable: React.FC<Props> = (props: Props) => {
                 <div className="w-6/12 text-center">
                     <span>
                         Page <span className="bg-white rounded border border-gray-300 px-4 py-2">{page}</span> of{" "}
-                        {projectsData.pagesTotal}
+                        {getNumberPages(projectsData, constants.pageSize)}
                     </span>
                 </div>
                 <button
                     onClick={() => {
-                        if (projectsData.nextPage) {
+                        if (nextPageAvailable(projectsData, constants.pageSize, page)) {
                             setPage((a) => a + 1);
                         }
                     }}
-                    disabled={projectsData === undefined || !projectsData.nextPage}
+                    disabled={!nextPageAvailable(projectsData, constants.pageSize, page)}
                     className="bg-gray-100 hover:bg-gray-200 border py-4 m-1 w-3/12 disabled:opacity-50 disabled:cursor-default"
                 >
                     Next
