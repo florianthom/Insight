4db2e06551c0d742d0c0635c3f5dc872b1053f0f
florianthom <thom.florian@yahoo.de>
2024-01-25 18:30:57 +0100

    Several changes

--- src/app/shared/meta/meta.tsx
+++ src/app/shared/meta/meta.tsx
@@ -12,7 +12,7 @@ export const Meta: React.FC<Props> = (props: Props) => (
 
         <link rel="icon" href="/static/images/favicon.ico" />
         <meta key="og:title" property="og:title" content="Florian Thom" />
-        <meta key="og:description" property="og:description" content="Freelancer covering professional services in IT and Media." />
+        <meta key="og:description" property="og:description" content="Freelancer covering professional services in IT and Media. From webapps to cloud expertise I offer tailored solutions to elevate your business." />
         <meta key="og:image" property="og:image" content="https://www.florianthom.com/static/images/florianthom.jpg" />
         <meta key="og:url" property="og:url" content="https://www.florianthom.com" />
         <meta key="og:type" property="og:type" content="website" />
